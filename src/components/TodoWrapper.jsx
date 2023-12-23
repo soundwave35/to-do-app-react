ad2a5a0619918289e337cca1cf847abf9d9d825e
Yarkın Bakkaloğlu <yark38@gmail.com>
2023-12-23 16:39:07 +0300

    Adding Todos and Using Props

--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import { TodoForm } from "./TodoForm";
+import {v4 as uuidv4} from "uuid";
+import { Todo } from "./Todo";
+uuidv4();
 
 export const TodoWrapper = () => {
+
+const [todos, setTodos] = useState([])
+
+const addTodo = (todo) => {
+  setTodos([...todos, {
+    id: uuidv4(),
+    task: todo,
+    completed: false,
+    isEditing: false
+  }])
+  console.log(todos)
+}
+
   return (
     <div className="TodoWrapper">
-      <TodoForm />
+      <h1>Todo List</h1>
+      <TodoForm addTodo={addTodo}/>
+      {todos.map((todo, index) => (
+        <Todo task={todo} key={index} />
+      ))}
     </div>
   )
 }
